e81e2c135d690e3a993aac822dda1670b23cba7b
kodok-ijho <dyudhiantoro@gmail.com>
2023-05-13 14:35:44 +0000

    - Finish and successfull submit Day 3 - Exercise 5 on CodeRoom

--- CodingRoom/Day 3 - Excercise 5.py
+++ CodingRoom/Day 3 - Excercise 5.py
@@ -5,3 +5,29 @@ name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+#name1Lower = name1.lower()
+#name2Lower = name2.lower()
+nameLower = name1.lower() + name2.lower()
+
+countTrue = 0
+countLove = 0
+
+countTrue = countTrue + nameLower.count("t")
+countTrue = countTrue + nameLower.count("r")
+countTrue = countTrue + nameLower.count("u")
+countTrue = countTrue + nameLower.count("e")
+
+countLove = countLove + nameLower.count("l")
+countLove = countLove + nameLower.count("o")
+countLove = countLove + nameLower.count("v")
+countLove = countLove + nameLower.count("e")
+
+Total = (countTrue * 10) + countLove
+
+if Total < 10 or Total > 90:
+    print(f"Your score is {Total}, you go together like coke and mentos.")
+elif Total > 40 and Total < 50:
+    print(f"Your score is {Total}, you are alright together.")
+else:
+    print(f"Your score is {Total}.")
